aa1e50665eb35a37a37700b62fbe30568714bcf0
AsrarJarvis <63350882+AsrarJarvis@users.noreply.github.com>
2022-09-22 00:51:51 +0530

    navbar and moviecard component added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import './App.css';
+import {data} from './data'
+import MovieCard from './MovieCard';
+import NavBar from './NavBar';
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <NavBar/>
+      <div className='main'>
+        <div className='tabs'>
+          <div className='tab'>Movies</div>
+          <div className='tab'>Favourites</div>
+        </div>
+        <div className='list'>
+          {data.map((movie)=>(
+            <MovieCard movie={movie} />
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
